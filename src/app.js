6bb3a8e60f97f215af42a234bc969d02ac389338
james tak <jtak93@gmail.com>
2016-12-09 17:25:31 -0800

    add src

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+require('./scss/main.scss');
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>whats up Errybudy!?</h1>
+        <h2>sup</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
