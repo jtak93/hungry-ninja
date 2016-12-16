076bbcc198c5bef031bd136ca8af050ce224f0bc
james tak <jtak93@gmail.com>
2016-12-15 18:40:17 -0800

    implement material-ui and basic navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import NavBar from './appbar/appbar';
 import "../scss/main.scss";
+
 
 // app component
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   // render
   render() {
+
     return (
-      <div className="container">
+      <div>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+          <NavBar />
+        </MuiThemeProvider>
         {this.props.children}
       </div>
     );
