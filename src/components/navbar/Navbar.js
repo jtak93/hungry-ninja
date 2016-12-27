dbf75812119ad0286021b7e375027061181114f3
james tak <jtak93@gmail.com>
2016-12-27 14:21:47 -0800

    react materialize

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
-const Navbar = (props) => {
+import {Navbar, NavItem} from 'react-materialize'
+const navbarInstance = (props) => {
   return (
-    <nav>
-      {props.title}
-    </nav>
+    <Navbar brand={props.title} right>
+      <NavItem href='get-started.html'>About</NavItem>
+      <NavItem href='components.html'>Menu</NavItem>
+      <NavItem href='components.html'>Locations</NavItem>
+      <NavItem href='components.html'>Order Online</NavItem>
+    </Navbar>
   )
 }
 
-export default Navbar;
+export default navbarInstance;
