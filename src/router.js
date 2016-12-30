55f1dc8d1d632c0797929e61d0b071798914dac8
james tak <jtak93@gmail.com>
2016-12-29 16:29:05 -0800

    change component directory structure to page set up and set up react router links in nav bar

--- src/router.js
+++ src/router.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Router, Route, IndexRoute } from "react-router";
 import { history } from "./store.js";
 import App from "./components/App";
-import Home from "./components/Home";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Menu from "./components/menu/Menu";
+import Locations from "./components/locations/Locations";
+import Order from "./components/order/Order";
 import NotFound from "./components/NotFound";
 
 // build the router
@@ -10,6 +14,10 @@ const router = (
   <Router onUpdate={() => window.scrollTo(0, 0)} history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/menu" component={Menu}/>
+      <Route path="/locations" component={Locations}/>
+      <Route path="/order" component={Order}/>
       <Route path="*" component={NotFound}/>
     </Route>
   </Router>
